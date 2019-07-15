ec721617653ed23ee03df4609f035317031c3f94
Justin <justinyang1000@hotmail.com>
2019-07-15 09:32:24 -0400

    Initial commit

---
+++ src/main/java/com/jy/jumbo/JumboApplication.java
@@ -0,0 +1,13 @@
+package com.jy.jumbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JumboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JumboApplication.class, args);
+    }
+
+}
